238cccd3c16052ee46b1a843b753eb4a4b68adc2
Hikoyu <azrianpintar@gmail.com>
2024-10-07 05:55:08 +0700

    feat: added new feature

--- pages/lib/store.js
+++ pages/lib/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import paginationReducer from './features/main'
+import DialogReducer from './features/dialog'
+import SettingReducer from './features/bgm'
 
 export default configureStore({
   reducer: {
-    pagination: paginationReducer
+    pagination: paginationReducer,
+    dialog: DialogReducer,
+    setting: SettingReducer,
   }
 })
