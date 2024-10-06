3e92a71488a7a3fde185fee8a3c9dd11e5c40eb0
Hikoyu <azrianpintar@gmail.com>
2024-10-07 06:03:52 +0700

    feat: new setup container and audioPlayer

--- pages/index.page.js
+++ pages/index.page.js
@@ -1,14 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
 import Frame1 from "./frame/introduction/index.page";
 import Frame from "./frame/index.page";
 
-const metadata = {
-  title: "Flower: crash and grow",
-};
+import metadata from "./metadata";
+import Component from "./component/index.page";
+
+import { Howler, Howl } from "howler";
+
+import { useSelector } from "react-redux";
+import { useRef, useEffect } from "react";
 
 export default function Home() {
+  const { volume } = useSelector((state) => state.setting);
+
+  const audioRef = useRef(null); 
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume;
+    }
+  }, [volume]);
+
   return (
     <>
       <Head>
@@ -17,10 +31,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={styles.page}
-      >
-        <Frame/>
+      <audio autoPlay ref={audioRef} hidden loop src="audio/main.mp3" />
+      <Component />
+      <div className={styles.page}>
+        <Frame />
       </div>
     </>
   );
