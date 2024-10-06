ed4d7ee66f3713e72ce9f538d9e0ed64f68a2bf3
Hikoyu <azrianpintar@gmail.com>
2024-10-07 05:42:07 +0700

    hotfix: change type file config

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: 'file-loader',
+      },
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
