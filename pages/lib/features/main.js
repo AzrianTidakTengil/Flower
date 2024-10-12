debddcd15b60abe3cb160a03d8c5fdb034210805
Hikoyu <azrianpintar@gmail.com>
2024-10-12 18:35:20 +0700

    feat: add new content isStart and status

--- pages/lib/features/main.js
+++ pages/lib/features/main.js
@@ -3,6 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const initialState = {
   page: 1,
   frame: 1,
+  isStart: false,
+  status: null,
 };
 
 const paginationSlice = createSlice({
@@ -10,16 +12,23 @@ const paginationSlice = createSlice({
   initialState,
   reducers: {
     setPage(state, action) {
-      state.page = action?.payload;
+      if (state.page !== action?.payload) {
+        state.status = 'waiting'
+      } else {
+        state.page = action?.payload;
+      }
     },
     setFrame(state, action) {
-      state.handleFrame = actioin?.payload?.handleFrame;
-    }
+      state.page = action?.payload;
+    },
+    toggleStart(state, action){
+      state.isStart = true
+    },
   },
 });
 
 
-export const { setPage, setFrame } = paginationSlice.actions
+export const { setPage, setFrame, toggleStart, progresStatus } = paginationSlice.actions
 
 const paginationReducer = paginationSlice.reducer;
 export default paginationReducer
